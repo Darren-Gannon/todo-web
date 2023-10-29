b050f87af00cb4fde4db55278a5a49a433c656a8
darren_93 <darrengannon93@gmail.com>
2023-10-29 21:09:34 +0200

    fix: double load

--- src/app/board/pages/board-page/board-page.component.ts
+++ src/app/board/pages/board-page/board-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute } from '@angular/router';
-import { EMPTY, Subject, Subscription, map, mergeMap, switchMap, tap, throwError } from 'rxjs';
+import { EMPTY, Subject, Subscription, map, mergeMap, share, switchMap, tap, throwError } from 'rxjs';
 import { Board, BoardService, State, StateService, Task, TaskService } from '../../../../api';
 import { CachedResult } from '../../../../api/cache-result';
 import { TaskDialogComponent, TaskDialogResult } from './task-dialog/task-dialog.component';
@@ -24,6 +24,7 @@ export class BoardPageComponent implements OnInit, OnDestroy {
 
   public readonly board$ = this.boardId$.pipe(
     switchMap(boardId => this.boardService.findOne(boardId)),
+    share(),
   );
 
   public readonly states$ = this.boardId$.pipe(
